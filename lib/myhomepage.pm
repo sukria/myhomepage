b3d773a8170268b17567fb597bbd65a60d4f1b40
Alexis Sukrieh <sukria@sukria.net>
2023-10-20 15:47:59 +0200

    First version of the home page, activities are taken from the config file.

--- lib/myhomepage.pm
+++ lib/myhomepage.pm
@@ -3,8 +3,20 @@ use Dancer2;
 
 our $VERSION = '0.1';
 
+# make app_setting available in the templates
+hook before_template => sub {
+    my $tokens = shift;
+
+    # Put the app_setting from the configuration into the tokens
+    $tokens->{app_setting} = config->{'app_setting'};
+};
+
+
 get '/' => sub {
-    template 'index' => { 'title' => 'myhomepage' };
+    template 'index' => { 
+        title => 'myhomepage',
+        activities => config->{app_setting}->{activities},
+    };
 };
 
 true;
