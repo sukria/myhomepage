821b427998a70edd234b4db385b5cb9ea6795c93
Alexis Sukrieh <sukria@gmail.com>
2023-11-05 09:42:33 +0100

    update.sh checks for cpan deps

--- update.sh
+++ update.sh
@@ -3,6 +3,29 @@
 # Exit script if any command fails
 set -e
 
+# File containing the dependencies
+DEPS_FILE="docker.cpandeps"
+
+# Function to check if a module is available on CPAN with the specified version
+check_module() {
+  local module=$1
+  local version=$2
+  if cpanm --info "$module" | grep -q "$version"; then
+    echo "Module $module version $version is available on CPAN."
+  else
+    echo "Module $module version $version is NOT available on CPAN."
+    exit 1
+  fi
+}
+
+# Check each module listed in the dependencies file
+while IFS=';' read -r module version; do
+  check_module "$module" "$version"
+done < "$DEPS_FILE"
+
+# If all modules are available, continue with the update process
+echo "All modules are available on CPAN. Starting the update process."
+
 # Pull the latest changes from the Git repository
 git pull origin master
 
